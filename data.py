4a418b1da3a7463b076fc668716e1d3b699f2a2d
fneubert <fariz.neubert@gmail.com>
2019-03-19 08:53:54 +0100

    -

--- data.py
+++ data.py
@@ -7,38 +7,40 @@ import numpy as np
 import pandas as pd
 from pandas.io.json import json_normalize
 
+def get_sales_data():
+    private_key = "AhNpKGH4ToJBtmiAdjiSW3SFG1lej3x6UeNQRJn5"
+    callerId = 'fariz.neubert'
 
-"""
-Make a sample call to the Booli API asking for all listings in 'Nacka' in JSON format, 
-using 'YOUR_CALLER_ID' and 'YOUR_PRIVATE_KEY' for authentication
-"""
+    timestamp = str(int(time.time()))
+    unique = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
+    hashstr = sha1((callerId + timestamp + private_key + unique).encode('utf-8')).hexdigest()
 
-private_key = "AhNpKGH4ToJBtmiAdjiSW3SFG1lej3x6UeNQRJn5"
-callerId = 'fariz.neubert'
+    headers = {
+        'Host': 'api.booli.se',
+        'Accept': 'application/vnd.booli-v2+json',
+        'Vary': 'Accept-Encoding',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive',
+        'User - Agent': 'Mozilla / 5.0(Macintosh;'
+                        'Intel Mac OS X 10_14_3) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 72.0.3626.96 Safari / 537.36'
+    }
 
-timestamp = str(int(time.time()))
-unique = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
-hashstr = sha1((callerId + timestamp + private_key + unique).encode('utf-8')).hexdigest()
+    BASE_URL = 'https://api.booli.se/'
+    limit = 500
+    offset = 0
+    url = BASE_URL + 'sold?areaId=1&limit=' + str(limit) + '&offset=' + str(offset) + '&minPublished=20150301&callerId='\
+          + callerId + '&time=' + timestamp + "&unique=" + unique + '&hash=' + hashstr
 
-headers = {
-'Host': 'api.booli.se',
-#'Accept': 'application/vnd.booli-v2+json',
-'Vary': 'Accept-Encoding',
-'Content-Type': 'application/json',
-'Connection': 'keep-alive',
-    'User - Agent': 'Mozilla / 5.0(Macintosh;'
-                    'Intel Mac OS X 10_14_3) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 72.0.3626.96 Safari / 537.36'
-}
-url = 'https://api.booli.se//sold?areaId=1&offset=35&callerId=' + callerId + '&time=' + timestamp + "&unique=" + unique + '&hash=' + hashstr
+    response = requests.get(url, headers=headers)
 
-response = requests.get(url, headers=headers)
+    if (response.status_code != 200):
+        print("fail")
+        print(response.text)
+        print(response.status_code)
 
-if(response.status_code != 200):
-    print("fail")
-    print(response.text)
-    print(response.status_code)
+    result = response.json()
+    sales_data = json_normalize(result['sold'])
 
-result = response.json()
+    return sales_data, result
 
-sales_data = json_normalize(result['sold'])
-
+sales, result = get_sales_data()
